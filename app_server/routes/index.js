848e331a6d6d25a08f3a25f3613f1fa45095c102
Richard Ighodaro <richard.ighodaro@hotmail.co.uk>
2015-05-03 17:05:34 +0100

    updated index.js

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+var homepageController = function (req, res) {
+  res.render('index', { title: 'Express' });
+};
+
+router.get('/', homepageController);
+
+/* GET home page.
+router.get('/', function(req, res, nex) {
   res.render('index', { title: 'Express' });
 });
+
+*/
 
 module.exports = router;
