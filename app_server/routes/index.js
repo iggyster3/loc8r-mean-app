04a443a3c0233093e12a7fc3f367b5af487a8adf
Richard Ighodaro <richard.ighodaro@hotmail.co.uk>
2015-05-04 09:41:29 +0100

    updated routes/index.js

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var ctrlMain = require('../controllers/main');
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/others');
+
 
 /*
 var homepageController = function (req, res) {
@@ -9,8 +12,14 @@ var homepageController = function (req, res) {
 */
 
 /* GET home page. */
-
 router.get('/', ctrlMain.index);
 
+/* GET Locations page. */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+/* Other page. */
+router.get('/about', ctrlMain.about);
 
 module.exports = router;
